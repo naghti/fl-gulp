f92470c0f2a5b191c0f309cdbe16135bb788b1d2
naghti <57380294+naghti@users.noreply.github.com>
2021-07-21 11:22:43 +0700

    Update alert.js

--- #src/js/alert.js
+++ #src/js/alert.js
@@ -1,16 +1,1 @@
-function testWebP(callback) {
-    var webP = new Image();
-    webP.onload = webP.onerror = function () {
-        callback(webP.height == 2);
-    };
-    webP.src =
-        "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-}
-
-testWebP(function (support) {
-    if (support == true) {
-        document.querySelector("body").classList.add("webp");
-    } else {
-        document.querySelector("body").classList.add("no-webp");
-    }
-});
+function testWebP(b){var a=new Image();a.onload=a.onerror=function(){b(a.height==2)};a.src="data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA"}testWebP(function(a){if(a==true){document.querySelector("body").classList.add("webp")}else{document.querySelector("body").classList.add("no-webp")}});
